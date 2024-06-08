359d4c795b1eb47a99757af369806c44e4059a98
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-08 16:21:08 +0300

    Added reference.

--- src/main/java/ru/albina/planner/client/ReferenceClient.java
+++ src/main/java/ru/albina/planner/client/ReferenceClient.java
@@ -4,12 +4,12 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.albina.backlib.configuration.WebConstants;
-import ru.albina.planner.dto.medical.Doctor;
 import ru.albina.planner.dto.reference.GetOrGenerateYearlyWorkloadRequest;
 import ru.albina.planner.dto.reference.HoursPerMonthValue;
+import ru.albina.planner.dto.reference.WeekNumberResult;
 import ru.albina.planner.dto.reference.Workload;
 
-import java.util.Collection;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,10 +37,18 @@ public class ReferenceClient {
 
     public double getHours(int year, int month) {
         return this.webClient.get()
-                .uri( uriBuilder->uriBuilder.path(WebConstants.FULL_PRIVATE + "/hours-per-month/{year}/months/{month}").build(year, month))
+                .uri(uriBuilder -> uriBuilder.path(WebConstants.FULL_PRIVATE + "/hours-per-month/{year}/months/{month}").build(year, month))
                 .retrieve()
                 .bodyToMono(HoursPerMonthValue.class)
                 .block()
                 .getHours();
+    }
+
+    public WeekNumberResult getWeek(LocalDate localDate) {
+        return this.webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(WebConstants.FULL_PRIVATE + "/week-numbers").queryParam("date", localDate).build())
+                .retrieve()
+                .bodyToMono(WeekNumberResult.class)
+                .block();
     }
 }
--- src/main/java/ru/albina/planner/service/planner/PlannerWorkloadService.java
+++ src/main/java/ru/albina/planner/service/planner/PlannerWorkloadService.java
@@ -7,12 +7,9 @@ import ru.albina.planner.dto.planner.WorkloadDto;
 import ru.albina.planner.dto.reference.GetOrGenerateYearlyWorkloadRequest;
 import ru.albina.planner.mapper.ReferenceMapper;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,10 +17,6 @@ import java.util.stream.IntStream;
 @Service
 @RequiredArgsConstructor
 public class PlannerWorkloadService {
-    private static final Locale RUSSIAN = new Locale("ru", "RUS");
-
-    private static final WeekFields RUSSIAN_WEEK = WeekFields.of(RUSSIAN);
-
 
     private final ReferenceClient referenceClient;
 
@@ -33,25 +26,29 @@ public class PlannerWorkloadService {
 
         final var endDate = startDate.withDayOfMonth(startDate.getMonth().length(startDate.isLeapYear()));
 
-        final int startWeekNumber = startDate.get(RUSSIAN_WEEK.weekOfWeekBasedYear());
+        final var weekNumberResult = this.referenceClient.getWeek(startDate);
+        final var endWeekNumberResult = this.referenceClient.getWeek(endDate);
 
         final var request = new ArrayList<GetOrGenerateYearlyWorkloadRequest>();
 
-        request.add(
-                GetOrGenerateYearlyWorkloadRequest.builder()
-                        .year(startDate.getYear())
-                        .weeks(IntStream.range(startWeekNumber, endDate.get(RUSSIAN_WEEK.weekOfWeekBasedYear()) + 1).boxed().collect(Collectors.toSet()))
-                        .build()
-        );
-
-
-        if (startWeekNumber == 1 && startDate.getDayOfWeek() != DayOfWeek.MONDAY) {
-            //contains prev year
-            final var week = startDate.minusDays(7).get(RUSSIAN_WEEK.weekOfWeekBasedYear());
+        if (weekNumberResult.getStartDate().getYear() != startDate.getYear()) {
             request.add(
                     GetOrGenerateYearlyWorkloadRequest.builder()
                             .year(startDate.getYear() - 1)
-                            .weeks(Set.of(week))
+                            .weeks(Set.of(weekNumberResult.getWeekNumber()))
+                            .build()
+            );
+            request.add(
+                    GetOrGenerateYearlyWorkloadRequest.builder()
+                            .year(startDate.getYear())
+                            .weeks(IntStream.range(1, endWeekNumberResult.getWeekNumber() + 1).boxed().collect(Collectors.toSet()))
+                            .build()
+            );
+        } else {
+            request.add(
+                    GetOrGenerateYearlyWorkloadRequest.builder()
+                            .year(startDate.getYear())
+                            .weeks(IntStream.range(weekNumberResult.getWeekNumber(), endWeekNumberResult.getWeekNumber() + 1).boxed().collect(Collectors.toSet()))
                             .build()
             );
         }
