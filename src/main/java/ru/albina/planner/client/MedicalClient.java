3e46cf5de56b594b4281829f09e22a895fd45efb
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-11 00:32:07 +0300

    Up exchange

--- src/main/java/ru/albina/planner/client/MedicalClient.java
+++ src/main/java/ru/albina/planner/client/MedicalClient.java
@@ -2,6 +2,7 @@ package ru.albina.planner.client;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.albina.backlib.configuration.WebConstants;
 import ru.albina.planner.dto.medical.Doctor;
@@ -15,7 +16,13 @@ public class MedicalClient {
     private final WebClient webClient;
 
     public MedicalClient(WebClient.Builder libWebClientBuilder) {
+        final int size = 30 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+                .build();
+
         this.webClient = libWebClientBuilder
+                .exchangeStrategies(strategies)
                 .baseUrl(Optional.ofNullable(System.getenv("MEDICAL_SERVICE_HOST")).orElse("http://localhost:8081"))
                 .build();
     }
